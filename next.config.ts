08580dcb53bc3544ab2a6bfe44b6388f6f21bcdd
Yogesh-Analytics <yogeshfulari2004@gmail.com>
2026-01-13 16:01:45 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.telegraphindia.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tse1.mm.bing.net",
+      },
+      {
+        protocol: "https",
+        hostname: "static.toiimg.com", // ✅ Added TOI image host
+      },
+    ],
+  },
+};
+
+export default nextConfig;
